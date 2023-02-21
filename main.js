4f7f68ce5724a69c93ab21b8e17a8be7685c92eb
Пользователь <TokhirovIlhom>
2023-02-21 23:10:33 +0500

    JavaScript4-dars

---
+++ main.js
@@ -0,0 +1,183 @@
+//1-misol
+
+// let day = 1;
+
+// switch (day) {
+//     case 1 :
+//         console.log('Du');
+//         break;
+//     case 2 :
+//         console.log('Se');
+//         break;
+//     case 3 :
+//         console.log('Cho');
+//         break;
+//     case 4 :
+//         console.log('Pa');
+//         break; 
+//     case 5 :
+//         console.log('Ju');
+//         break;
+//     case 6 :
+//         console.log('Sha');
+//         break;
+//     case 7 :
+//         console.log('Ya');
+//         break;
+// }
+
+//2-misol
+
+// let baho = 3
+
+// switch (baho) {
+//     case 1:
+//         console.log('yomon');
+//         break;
+//     case 2:
+//         console.log('qoniqarsiz');
+//         break;
+//     case 3: 
+//         console.log('qoniqarli');
+//         break;
+//     case 4:
+//         console.log('yaxshi');
+//         break;
+//     case 5: 
+//         console.log('alo');
+//         break;
+//         default:
+//             console.log('demak siz ota olmadingiz');
+//             break;
+// }
+
+//3-misol
+
+// let oy = 4
+
+// switch (oy) {
+//     case 12:
+//         console.log('Yanvar');
+//         break;
+//     case 1:
+//         console.log('Yanvar');
+//         break;
+//     case 2:
+//         console.log('Yanvar');
+//         break;
+//     case 3:
+//         console.log('Bahor');
+//         break;
+//     case 4:
+//         console.log('Bahor');
+//         break;
+//     case 5:
+//         console.log('Bahor');
+//         break;
+//     case 6:
+//         console.log('Yoz');
+//         break;
+//     case 7:
+//         console.log('Yoz');
+//         break;
+//     case 8:
+//         console.log('Yoz');
+//         break;
+//     case 9:
+//         console.log('Kuz');
+//         break;
+//     case 10:
+//         console.log('Kuz');
+//         break;
+//     case 11:
+//         console.log('Kuz');
+//         break;
+//         default:
+//             console.log('Bunday fasl yoq');
+//             break;
+// }
+
+//4-misol
+
+// let oy = 1
+
+// switch (oy) {
+//     case 1:
+//         console.log(31);
+//         break;
+//     case 2:
+//         console.log(28);
+//         break;
+//     case 3:
+//         console.log(31);
+//         break;
+//     case 4:
+//         console.log(30);
+//         break;
+//     case 5:
+//         console.log(31);
+//         break;
+//     case 6:
+//         console.log(30);
+//         break;
+//     case 7:
+//         console.log(31);
+//         break;
+//     case 8:
+//         console.log(31);
+//         break;
+//     case 9:
+//         console.log(30);
+//         break;
+//     case 10:
+//         console.log(31);
+//         break;
+//     case 11:
+//         console.log(30);
+//         break;
+//     case 12:
+//         console.log(31);
+//         break;
+// }
+
+//5-misol
+
+// function getInfo(son1, son2, amal) {
+//     if (amal == "+") {
+//         console.log(son1 + son2);
+//     } else if (amal == "-"){
+//         console.log(son1 - son2);
+//     } else if (amal == "*") {
+//         console.log(son1 * son2);
+//     } else if (amal == "/") {
+//         console.log(son1 / son2);
+//     }
+    
+// }
+
+// getInfo(244, 43, "/")
+
+//6-misol
+
+// let baho = 3
+
+// switch (baho) {
+//     case 1:
+//         console.log(10000);
+//         break;
+//     case 2:
+//         console.log(20000);
+//         break;
+//     case 3:
+//         console.log(30000);
+//         break;
+//     case 4:
+//         console.log(40000);
+//         break;
+//     case 5:
+//         console.log(50000);
+//         break;
+//     default:
+//         console.log('Sizga 1tiyin ham yoq');
+//         break;
+// }
